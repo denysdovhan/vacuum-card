033f3b78e00e3b90772010cc408e9c47edcd0443
MrBartusek <bartusekcraft@gmail.com>
2021-05-28 16:44:31 +0200

    Fix

--- src/vacuum-card.js
+++ src/vacuum-card.js
@@ -100,7 +100,7 @@ class VacuumCard extends LitElement {
   }
 
   getCardSize() {
-    return this.config.compact_view || false ? 2 : 8;
+    return this.config.compact_view || false ? 3 : 8;
   }
 
   shouldUpdate(changedProps) {
